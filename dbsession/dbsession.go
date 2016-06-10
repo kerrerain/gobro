c46441fd5bf4a210c68ccae88c50c9578bffd9d0
magleff <mail.henry.bonjour@gmail.com>
2016-06-10 18:28:04 +0200

    Delegate the expenses management to another package

--- dbsession/dbsession.go
+++ dbsession/dbsession.go
@@ -2,8 +2,8 @@ package dbsession
 
 import (
 	"gopkg.in/mgo.v2"
-	"time"
 	"log"
+	"time"
 )
 
 // As seen in http://blog.mongodb.org/post/80579086742/running-mongodb-queries-concurrently-with-go
@@ -11,7 +11,7 @@ func Create() *mgo.Session {
 	mongoDBDialInfo := &mgo.DialInfo{
 		Addrs:    []string{"localhost:27017"},
 		Timeout:  60 * time.Second,
-		Database: "expenses-analyzer"}
+		Database: "gobro"}
 	// Create a session which maintains a pool of socket connections
 	// to our MongoDB.
 	mongoSession, err := mgo.DialWithInfo(mongoDBDialInfo)
--- gobro.go
+++ gobro.go
@@ -1,73 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"flag"
-	//	"fmt"
+	"github.com/magleff/gobro/expense"
 	"log"
 	"os"
-	"strconv"
-	"strings"
-	"time"
-	"gopkg.in/mgo.v2"
-	"github.com/magleff/gobro/dbsession"
 )
-
-type Entry struct {
-	Date        time.Time
-	Description string
-	Amount      float32
-}
-
-func parseTime(input string) time.Time {
-	time, err := time.Parse("02/01/2006", input)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return time
-}
-
-func parseAmount(amount string) float32 {
-	amount = strings.Replace(amount, ",", ".", 1)
-	amountFloat, err := strconv.ParseFloat(amount, 32)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return float32(amountFloat)
-}
-
-func processLine(line string) Entry {
-	fields := strings.Split(line, ";")
-	return Entry{parseTime(fields[0]), fields[1], parseAmount(fields[2])}
-}
-
-func importEntriesIntoDB(mongoSession *mgo.Session, entries []Entry) {
-	expenses := mongoSession.DB("").C("expenses")
-	for _, entry := range entries {
-		expenses.Insert(entry)
-	}
-}
-
-func processFile(file *os.File) {
-	scanner := bufio.NewScanner(file)
-	countLine := 0
-	var entries []Entry
-
-	for scanner.Scan() {
-		if countLine > 7 {
-			entries = append(entries, processLine(scanner.Text()))
-		}
-		countLine++
-	}
-
-	mongoSession := dbsession.Create()
-
-	importEntriesIntoDB(mongoSession, entries)
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-}
 
 func parseFileName() string {
 	fileFlag := flag.String("f", "", "The path to the file")
@@ -82,5 +20,5 @@ func main() {
 	}
 	defer file.Close()
 
-	processFile(file)
+	expense.ImportFromFile(file)
 }
