529a9f3ea126897372878424c8cef3f7538e1da8
magleff <mail.henry.bonjour@gmail.com>
2016-06-25 11:00:14 +0200

    Gives the status of the current budget

--- cmd/status.go
+++ cmd/status.go
@@ -1,10 +1,41 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/magleff/gobro/database"
 	"github.com/magleff/gobro/features/budget"
 	"github.com/spf13/cobra"
 )
+
+func computeEarnings(budget budget.Budget) float32 {
+	earnings := float32(0.00)
+	for _, entry := range budget.Expenses {
+		if entry.Amount > 0 {
+			earnings += entry.Amount
+		}
+	}
+	return earnings
+}
+
+func computeExpenses(budget budget.Budget) float32 {
+	expenses := float32(0.00)
+	for _, entry := range budget.Expenses {
+		if entry.Amount <= 0 {
+			expenses += entry.Amount
+		}
+	}
+	return expenses
+}
+
+func displayBudgetInfos(budget budget.Budget) {
+	earnings := computeEarnings(budget)
+	expenses := computeExpenses(budget)
+
+	fmt.Println("Created on", budget.StartDate)
+	fmt.Println("Total earnings", earnings)
+	fmt.Println("Total expenses", expenses)
+	fmt.Println("Balance", earnings+expenses)
+}
 
 var statusCmd = &cobra.Command{
 	Use:   "status",
@@ -13,7 +44,8 @@ var statusCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		DB := database.NewDatabase()
 		controller := budget.NewController(DB)
-		controller.CurrentBudget()
+		currentBudget := controller.CurrentBudget()
+		displayBudgetInfos(*currentBudget)
 	},
 }
 
