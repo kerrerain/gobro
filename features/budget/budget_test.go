a5d86556be7bfda5c931dd50adc5497e0a033141
magleff <mail.henry.bonjour@gmail.com>
2016-07-24 10:33:01 +0200

    Yet another test on budget (to be squashed)

--- features/budget/budget_test.go
+++ features/budget/budget_test.go
@@ -1,6 +1,7 @@
 package budget
 
 import (
+	"github.com/magleff/gobro/features/expensefixed"
 	"github.com/stretchr/testify/assert"
 	"testing"
 	"time"
@@ -14,4 +15,11 @@ func TestNewBudget(t *testing.T) {
 		budget.StartDate.Format("2006-01-02"),
 		"Should set the start date to the current date.")
 	assert.Equal(t, 0, len(budget.Expenses), "Should init an empty slice for expenses.")
+}
+
+func TestNewBudgetWithExpensesFixed(t *testing.T) {
+	expensesFixed := []expensefixed.ExpenseFixed{*expensefixed.NewExpenseFixed("60.50", "test"),
+		*expensefixed.NewExpenseFixed("50.25", "test2")}
+	budget := NewBudgetWithExpensesFixed(expensesFixed, "32.52")
+	assert.Equal(t, 2, len(budget.Expenses), "Should init the budget with fixed expenses")
 }
